c1fd24553a6843cedc0ce0544bd0b3be66c6df14
Shravan Parthe <shravanparthe@gmail.com>
2026-04-01 01:28:13 +0530

    Create eleven.dart

---
+++ eleven.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+/* margin and padding */
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold(
+      // body: Padding(
+      //   padding: EdgeInsetsGeometry.only(top:20,left: 34),
+      //   child: Text(
+      //   'Hello World',
+      //   style: TextStyle(
+      //     fontSize: 20
+      //   ),
+      // ),
+      // )
+      body: Container(
+        color:Colors.red,
+        margin: EdgeInsets.all(11),
+        child: Padding(
+          padding:EdgeInsetsGeometry.only(top:20),
+          child: Text('Hello World',style: TextStyle(fontSize: 25,color: Colors.blueAccent),),
+        ),
+      )
+    ));
+  }
+}
